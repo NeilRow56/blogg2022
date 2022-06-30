2bfe004ad3bd2aa7792d991692750142dfc14f6a
Neil Rowland <neilrowland56@yahoo.com>
2022-06-30 19:06:57 +0100

    Added Slider

--- components/Section1.js
+++ components/Section1.js
@@ -1,8 +1,17 @@
 import Image from "next/image"
 import Link from "next/link"
 import Author from "./_child/Author"
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Autoplay } from 'swiper'
+
+// Import Swiper styles
+import 'swiper/css';
 
 const Section1 = () => {
+
+    SwiperCore.use([Autoplay])
+
     const bg = {
         background: "url('/images/banner.png') no-repeat",
         backgroundPosition: "right"
@@ -12,7 +21,25 @@ const Section1 = () => {
         <div className="container mx-auto md:px-20">
             <h1 className="font-bold text-4xl pb-12 text-center">Trending</h1>
 
-            { Slide() }
+            <Swiper
+               
+                slidesPerView={1}
+                loop={true}
+                autoplay={{
+                    delay: 5000
+                }}
+                
+                >
+                <SwiperSlide>{ Slide() }</SwiperSlide>
+                <SwiperSlide>{ Slide() }</SwiperSlide>
+                <SwiperSlide>{ Slide() }</SwiperSlide>
+                <SwiperSlide>{ Slide() }</SwiperSlide>
+                <SwiperSlide>{ Slide() }</SwiperSlide>
+                
+      ...
+            </Swiper>
+
+            
 
 
         </div>
